0ccb7024e3c9d38ff0e3bf274882c3fd6bcdf539
vizline <biuro@viz-line.com>
2021-12-10 20:53:51 +0100

    Update text_variables.js

--- jsPsych_version/configuration/text_variables.js
+++ jsPsych_version/configuration/text_variables.js
@@ -1,58 +1,58 @@
 // instructions page 1
 var page1 = [
-  '<p>Your main task is to respond to white arrows (with a black border) that appear on the screen.</p> '+
-  '<p>Press the LEFT ARROW KEY with the right index finger when you see a LEFT ARROW and press the RIGHT ARROW KEY with the right ring finger when you see a RIGHT ARROW. </p>'+
-  '<p>Thus, left arrow = left key and right arrow = right key. </p>'+ '<br>' +
-  '<p>However, on some trials (stop-signal trials) the arrows will turn red after a variable delay. You have to stop your response when this happens. </p>'+
-  '<p>On approximately half of the stop-signal trials, the red stop signal will appear soon and you will notice that it will be easy to stop your response. </p>'+
-  '<p>On the other half of the trials, the red stop signals will appear late and it will become very difficult or even impossible to stop your response.</p>'
+  '<p>Twoim głównym zadaniem jest reagowanie na białe strzałki (z czarnym konturem), które będą się pojawiać na ekranie.</p> '+
+  '<p>Naciśnij LEWĄ strzałkę na klawiaturze kiedy zobaczysz LEWĄ strzałkę na ekranie, lub naciśnij PRAWĄ strzałkę na klawiaturze jeśli PRAWA strzałka pojawi się na ekranie.</p>'+
+  '<p>Więc: lewa strzałka = lewy przycisk, prawa strzałka = prawy przycisk.</p>'+ '<br>' +
+  '<p>Czasami prezentowane na ekranie strzałki będą z niewielkim opóźnieniem wypełniać się kolorem czerwonym. Wtedy powstrzymaj się od naciskania jakiegokolwiek przycisku!</p>'+
+  '<p>W ok. połowie przypadków czerwone wypełnienie pojawi się prawie natychmiast, co sprawi że powstrzymanie się od reakcji będzie bardzo łatwe.</p>'+
+  '<p>Natomiast w innych przypadkach czerwień pojawi się stosunkowo późno - więc powstrzymanie się od reakcji będzie trudne.</p>'
 ];
 
 // instructions page 2
 // Do not forget to adjust the number of blocks
 page2 = [
-  '<p> Nevertheless, it is really important that you do not wait for the stop signal to occur and that you respond as quickly and as accurately as possible to the white arrows. </p>'+
-  '<p> After all, if you start waiting for the red stop signals, then the program will delay their presentation. This will result in long reaction times. </p>'+
-  '<p> We will start with a short practice block in which you will receive immediate feedback. You will no longer receive immediate feedback in the experimental phase. </p>'+
-  '<p> However, at the end of each experimental block, there will be a 15 second break. During this break, we will show you some information about your mean performance in the previous block.</p>' +
-  '<p> The experiment consists of 1 practice block and 4 experimental blocks</p>'
+  '<p> Prosimy, abyś udzielał swojej odpowiedzi tak szybko jak to jest możliwe - zadanie to ma na celu zmierzenie czasu reakcji na prezentowane bodźce. </p>'+
+  '<p> Powinieneś podejmować szybkie decyzje - aby Twoje czasy reakcji były jak najkrótsze, jednocześnie przy jak najmniejszej ilości błędów (naciskaniu klawiszy gdy strzałka będzie czerwona).</p>'+
+  '<p> Zadanie rozpocznie się jednym treningowym blokiem bodźców, gdy będziesz otrzymywać informacje zwrotne na temat poprawności swoich działań. Później, takich informacji zwrotnych już nie będzie.</p>'+
+  '<p> Niemniej jednak, po każdym z "właściwych" bloków będzie 15 sekund przerwy - gdy otrzymasz informacje nt. swoich osiągnięć w poprzedniej serii bodżców.</p>' +
+  '<p> To badanie składa się z jednego bloku treningowego - oraz 4 właściwych bloków z bodżcami. Wykonanie całości zadania powinno zająć Ci ok. 6 minut. </p>'
 ];
 
 // informed consent text
 var informed_consent_text = [
-  '<p> Type your informed consent text in the text_variables.js file... </p>'
+  '<p> Jestem gotowy/a, aby rozpocząć zadanie.</p>'
 ];
 
 // trial by trial feedback messages
-correct_msg = '<p> correct response </p>'
-incorrect_msg = '<p> incorrect response </p>'
-too_slow_msg = '<p> too slow </p>'
-too_fast_msg = '<p> too fast </p>'
-correct_stop_msg = '<p> correct stop </p>'
-incorrect_stop_msg = '<p> remember: try to stop </p>'
+correct_msg = '<p> Poprawna reakcja </p>'
+incorrect_msg = '<p> Niepoprawna reakcja </p>'
+too_slow_msg = '<p> Zbyt wolno </p>'
+too_fast_msg = '<p> Zbyt szybko </p>'
+correct_stop_msg = '<p> Poprawne powstrzymanie się od reakcji </p>'
+incorrect_stop_msg = '<p> Nic nie naciskaj przy czerwonej strzałce! </p>'
 
 // block feedback
-no_signal_header = "<p><b>GO TRIALS: </b></p>"
-avg_rt_msg = "<p>Average response time = %d milliseconds</p>"
+no_signal_header = "<p><b>Próby z białą strzałką: </b></p>"
+avg_rt_msg = "<p>Średni czas reakcji = %d milisekund</p>"
 prop_miss_msg = "<p>Proportion missed go = %.2f (should be 0)</p>"
-stop_signal_header = "<p><b>STOP-SIGNAL TRIALS: </b></p>"
-prop_corr_msg = "<p>Proportion correct stops = %.2f (should be close to 0.5)</p>" + "<br>"
-next_block_msg = "<p>You can take a short break, the next block starts in <span id='timer'>15</span></p>"
-final_block_msg = "<p>Press space to continue...</p>" // after the final block they don't need a break
+stop_signal_header = "<p><b>Próby z czerwoną strzałką: </b></p>"
+prop_corr_msg = "<p>Proporcja poprawnych powstrzymań reakcji = %.2f (powinno tu być co najmniej 0.5)</p>" + "<br>"
+next_block_msg = "<p>Możesz wziąć szybki oddech, następny blok rozpocznie się za <span id='timer'>15</span></p>"
+final_block_msg = "<p>Naciśnij spację aby kontynuować...</p>" // after the final block they don't need a break
 
 // other
-var label_previous_button = 'Previous';
-var label_next_button = 'Next';
-var label_consent_button = 'I agree';
-var full_screen_message = '<p>The experiment will switch to fullscreen mode when you push the button below. </p>';
-var welcome_message = ['<p>Welcome to the experiment.</p>' + '<p>Press "Next" to begin.</p>'];
-var not_supported_message = ['<p>This experiment requires the Chrome or Firefox webbrowser.</p>'];
-var subjID_instructions = "Enter your participant ID."
-var age_instructions = "Enter your age."
-var gender_instructions = "Choose your gender."
-var gender_options = ['Female','Male', 'Other', 'Prefer not to say']
-var text_at_start_block = '<p>Press space to begin.</p>'
-var get_ready_message = '<p>Get ready...</p>'
+var label_previous_button = 'Poprzedni';
+var label_next_button = 'Dalej';
+var label_consent_button = 'Zgadzam się';
+var full_screen_message = '<p>Procedura zostanie uruchomiona w trybie całego ekranu - kiedy naciśniesz poniższy przycisk: </p>';
+var welcome_message = ['<p>Witaj w procedurze badawczej.</p>' + '<p>Naciśnij "Dalej" aby rozpocząć.</p>'];
+var not_supported_message = ['<p>Ten ekperyment wymaga korzystania z przeglądarki Chrome albo Firefox.</p>'];
+var subjID_instructions = "Wprowadź swój numer użytkownika SONA ID."
+var age_instructions = "Wprowadź swój wiek."
+var gender_instructions = "Wskaż swoją płeć."
+var gender_options = ['kobieta','mężczyzna']
+var text_at_start_block = '<p>Naciśnij spację aby kontynuować.</p>'
+var get_ready_message = '<p>Przygotuj się...</p>'
 var fixation_text = '<div style="font-size:60px;">TEST</div>'
-var end_message = "<p>Thank you for your participation.</p>" +
-  "<p>Press space to finalize the experiment.</p>"
+var end_message = "<p>Dziękuję za Twój udział w tej procedurze.</p>" +
+  "<p>Naciśnij spację aby zakończyć badanie.</p>"
